984e502650117c6bdd323827f2529f7443327d9f
Priyanka <priyanka.e.03.12@gmail.com>
2025-11-30 15:28:03 +0000

    api key added

--- WeatherDashboard/src/App.jsx
+++ WeatherDashboard/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+  const city = "Hyderabad";
+  const currentWeatherData = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  // console.log(currentWeatherData);
   return (
-    <>
+    <div className="app">
       <h1>Weather Dashboard</h1>
-    </>
+      <form>
+        <input type="text" placeholder="Enter city name..." />
+        <button type="submit">Search</button>
+      </form>
+    </div>
   );
 }
 
