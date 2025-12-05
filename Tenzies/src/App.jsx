4cd9283997b4472ddfb47328674bca321070be80
Priyanka <priyanka.e.03.12@gmail.com>
2025-12-05 16:06:17 +0000

    tenzies roll button added

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice());
+  }
+
+  const diceElements = dice.map((el) => <Die value={el} />);
+
   return (
     <main>
       <h1>Tenzies</h1>
@@ -9,6 +22,10 @@ function App() {
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
