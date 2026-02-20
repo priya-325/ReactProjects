8e12704437448f03fb84da0d9074da8865a6ac38
Priyanka <priyanka.e.03.12@gmail.com>
2026-02-20 23:04:22 +0000

    layout

--- MemeGenerator/src/App.jsx
+++ MemeGenerator/src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-function App() {
-  return <h1>hello</h1>;
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
-
-export default App;
