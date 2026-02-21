847352c8805330c1cdee85a016c5d8c28b30ba55
Priyanka <priyanka.e.03.12@gmail.com>
2026-02-21 16:09:08 +0000

    memegenerator finished

--- MemeGenerator/src/components/Main.jsx
+++ MemeGenerator/src/components/Main.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Main() {
-  const [meme, setmeme] = useState({
+  const [meme, setMeme] = useState({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const newMemeUrl = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: newMemeUrl,
+    }));
+  }
+
   function handleChange(event) {
     // const { value } = event.target;
     const { value, name } = event.currentTarget;
-    setmeme((prev) => ({
+    setMeme((prev) => ({
       ...prev,
       [name]: value,
     }));
@@ -39,7 +57,7 @@ export default function Main() {
             value={meme.bottomText}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={meme.imageUrl} />
